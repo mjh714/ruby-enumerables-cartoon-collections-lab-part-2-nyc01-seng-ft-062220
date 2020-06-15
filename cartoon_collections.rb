bb803754c1ffd626c004697b1fa16dbc2ac4f221
Michael Horowitz <mjhorowitz714@yahoo.com>
2020-06-15 13:39:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,7 +23,7 @@ def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   
   planeteer_calls.detect do  |planeteer_call|
-    valid_calls == planeteer_call
+    valid_calls.include? planeteer_call
   end
 end
 
